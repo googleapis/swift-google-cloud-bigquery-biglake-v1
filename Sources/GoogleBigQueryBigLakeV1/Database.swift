89d1b8ef21542e83f0be9997b64fec402bccde05
Carlos O'Ryan <coryan@google.com>
2026-09-11 20:11:51 +0000

    feat: update librarian with serialize-enums-as-strings (#829)
    Update librarian to use strings when serializing enums. Fixed a number
    of unit / integration tests.

--- Sources/GoogleBigQueryBigLakeV1/Database.swift
+++ Sources/GoogleBigQueryBigLakeV1/Database.swift
@@ -211,8 +211,8 @@ public struct Database: Codable, Equatable, GoogleCloudWKT._AnyPackable,
     public func encode(to encoder: Encoder) throws {
       var container = encoder.singleValueContainer()
       switch self {
-      case .unspecified: return try container.encode(0)
-      case .hive: return try container.encode(1)
+      case .unspecified: return try container.encode("TYPE_UNSPECIFIED")
+      case .hive: return try container.encode("HIVE")
       case .unknownIntValue(let v): return try container.encode(v)
       case .unknownStringValue(let v): return try container.encode(v)
       }
