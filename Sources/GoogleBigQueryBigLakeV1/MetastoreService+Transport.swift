92c9dd77a00314062a66be0d172532c178a3ee7c
Carlos O'Ryan <coryan@google.com>
2026-09-16 13:55:59 -0400

    chore: update librarian circa 2026-09-16 (#913)
    Path parameter fields should be omitted in the body.

--- Sources/GoogleBigQueryBigLakeV1/MetastoreService+Transport.swift
+++ Sources/GoogleBigQueryBigLakeV1/MetastoreService+Transport.swift
@@ -690,9 +690,10 @@ extension Clients {
     public func renameTable(
       request: RenameTableRequest, options: GoogleCloudGax.RequestOptions
     ) async throws -> GoogleBigQueryBigLakeV1.Table {
-      let (path, query, configure) = try {
+      let (path, query, configure, omitted) = try {
         () throws -> (
-          Swift.String, [URLQueryItem], (inout GoogleCloudGax._HTTPClientRequest) -> Void
+          Swift.String, [URLQueryItem], (inout GoogleCloudGax._HTTPClientRequest) -> Void,
+          [Swift.String]
         ) in
         if let candidate = try { () throws -> (Swift.String, [URLQueryItem])? in
           guard
@@ -713,7 +714,7 @@ extension Clients {
           ]
           return (path, query)
         }() {
-          return (candidate.0, candidate.1, { $0.setMethod(.POST) })
+          return (candidate.0, candidate.1, { $0.setMethod(.POST) }, ["name"])
         }
         var paths: [GoogleCloudGax.PathMismatch] = []
         do {
@@ -736,7 +737,7 @@ extension Clients {
         percentEncodedPath: path, query: query, options: options)
       configure(&req)
       req.addHeader(name: GoogleCloudGax._HeaderNames.apiClient, value: Clients.clientHeader)
-      try req.setBody(json: request)
+      try req.setBody(json: request, omitting: omitted)
       return try await req.rpc(
         GoogleBigQueryBigLakeV1.Table.self, timeout: options.attemptTimeout
       ).get()
